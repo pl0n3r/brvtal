eb42d2de61cfd22f0913979a0ced599ecc032872
BRVTAL Automation <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-11 21:45:58 +0000

    chore: update release metadata [skip ci]

--- config/version.php
+++ config/version.php
@@ -6,6 +6,6 @@ declare(strict_types=1);
  * Update APP_VERSION and APP_BUILD for each production release.
  */
 const BRVTAL_APP_VERSION = '0.1.0';
-const BRVTAL_APP_BUILD = '3445c44';
+const BRVTAL_APP_BUILD = 'b3740ff';
 const BRVTAL_APP_ENV = 'PRODUCTION';
 const BRVTAL_RELEASE_DATE = '2026-09-11';
