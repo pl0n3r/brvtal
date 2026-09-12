abeb330f94779f653c1c7f53ee0a1191705987c9
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-11 20:33:40 -0500

    feat: load global search from canonical admin shell

--- discadmin/index.php
+++ discadmin/index.php
@@ -8,7 +8,8 @@ $html = (string)ob_get_clean();
 $build = defined('BRVTAL_APP_BUILD') ? rawurlencode((string)BRVTAL_APP_BUILD) : '';
 $suffix = $build !== '' ? '?v=' . $build : '';
 $enhancements = '<script src="/discadmin/content-health.js' . $suffix . '"></script>'
-    . '<script src="/discadmin/seo-metadata.js' . $suffix . '"></script>';
+    . '<script src="/discadmin/seo-metadata.js' . $suffix . '"></script>'
+    . '<script src="/discadmin/global-search.js' . $suffix . '"></script>';
 
 if (str_contains($html, '</body>')) {
     $html = str_replace('</body>', $enhancements . '</body>', $html);
