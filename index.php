ecbb603155eecf9a029d8871917505eaab603f45
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-13 19:02:48 -0500

    feat: load public discovery URL state

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $html = str_replace('<body data-scene="CORE">', '<body data-scene="CORE">' . "\n
 $html = str_replace('<main id="top">', '<main id="top" tabindex="-1">', $html);
 $html = str_replace('</head>', "  <link rel=\"stylesheet\" href=\"css/input-accessibility.css\">\n  <link rel=\"stylesheet\" href=\"css/mobile-events.css\">\n  <link rel=\"stylesheet\" href=\"css/hero-slider.css\">\n  <link rel=\"stylesheet\" href=\"css/hero-slider-v2.css\" data-hero-v2-public=\"1\">\n</head>", $html);
 $html = str_replace('<script src="js/app.js"></script>', "<script src=\"js/mobile-performance.js\"></script>\n  <script src=\"js/app.js\"></script>", $html);
-$html = str_replace('</body>', "  <script src=\"js/menu-accessibility.js\"></script>\n  <script src=\"js/input-accessibility.js\"></script>\n  <script src=\"js/mobile-events.js\"></script>\n  <script src=\"js/hero-slider.js\"></script>\n</body>", $html);
+$html = str_replace('</body>', "  <script src=\"js/menu-accessibility.js\"></script>\n  <script src=\"js/input-accessibility.js\"></script>\n  <script src=\"js/mobile-events.js\"></script>\n  <script src=\"js/hero-slider.js\"></script>\n  <script src=\"js/public-discovery-url-state.js\"></script>\n</body>", $html);
 $html = brvtal_public_optimize_font_stylesheet($html);
 $html = brvtal_public_optimize_home_images($html);
 $html = brvtal_public_version_assets($html, brvtal_deployment_short_sha());
