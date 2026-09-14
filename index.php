cdc871a0dfc2a6626944afce2d21cb258ac0065c
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-13 21:53:37 -0500

    perf: route public home through adaptive runtime loader

--- index.php
+++ index.php
@@ -31,7 +31,15 @@ $html = (string)file_get_contents(__DIR__ . '/index.html');
 $html = str_replace('<body data-scene="CORE">', '<body data-scene="CORE">' . "\n  <a class=\"skip-link mono\" href=\"#top\">SKIP TO CONTENT</a>", $html);
 $html = str_replace('<main id="top">', '<main id="top" tabindex="-1">', $html);
 $html = str_replace('</head>', "  <link rel=\"stylesheet\" href=\"css/input-accessibility.css\">\n  <link rel=\"stylesheet\" href=\"css/mobile-events.css\">\n  <link rel=\"stylesheet\" href=\"css/hero-slider.css\">\n  <link rel=\"stylesheet\" href=\"css/hero-slider-v2.css\" data-hero-v2-public=\"1\">\n</head>", $html);
-$html = str_replace('<script src="js/app.js"></script>', "<script src=\"js/mobile-performance.js\"></script>\n  <script src=\"js/app.js\"></script>", $html);
+$legacyRuntime = <<<'HTML'
+  <script src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollTrigger.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/lenis@1.3.4/dist/lenis.min.js"></script>
+  <script src="js/app.js"></script>
+  <script src="js/archive.js"></script>
+  <script src="js/public-media.js"></script>
+HTML;
+$html = str_replace($legacyRuntime, '  <script src="js/public-runtime-loader.js"></script>', $html);
 $html = str_replace('</body>', "  <script src=\"js/menu-accessibility.js\"></script>\n  <script src=\"js/input-accessibility.js\"></script>\n  <script src=\"js/mobile-events.js\"></script>\n  <script src=\"js/hero-slider.js\"></script>\n  <script src=\"js/public-discovery-url-state.js\"></script>\n</body>", $html);
 $html = brvtal_public_optimize_font_stylesheet($html);
 $html = brvtal_public_optimize_home_images($html);
