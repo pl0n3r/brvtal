c738217563f0acd39954ea3d385fa8fef47bae6b
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-17 13:56:50 -0500

    fix: associate Blog search label

--- discadmin/blog.php
+++ discadmin/blog.php
@@ -28,7 +28,8 @@ brvtal_admin_require();
   </div>
 
   <div class="toolbar blog-toolbar">
-    <input class="search" id="blog-search" aria-label="Search blog posts" placeholder="Search title, excerpt or slug…" autocomplete="off">
+    <label class="admin-sr-only" for="blog-search">Search blog posts</label>
+    <input class="search" id="blog-search" placeholder="Search title, excerpt or slug…" autocomplete="off">
     <select id="blog-status-filter" aria-label="Filter posts by status">
       <option value="">ALL STATUS</option>
       <option value="published">PUBLISHED</option>
