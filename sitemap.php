cea7b38a1b79d30d53fa7bac6fa7c4c8efe7533f
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-17 16:52:35 -0500

    refactor: render sitemap through validated XML helper

--- sitemap.php
+++ sitemap.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/config/bootstrap.php';
 require_once __DIR__ . '/config/public_seo.php';
+require_once __DIR__ . '/config/public_sitemap.php';
 
 $base = brvtal_public_base_url($config);
 $urls = [];
@@ -45,30 +46,11 @@ foreach (brvtal_public_content_definitions() as $route => $definition) {
         if ($requiresEventVisibility && !brvtal_public_event_is_visible($row)) {
             continue;
         }
-        $location = $base . '/' . $route . '/' . rawurlencode((string)$row['slug']);
+        $location = $base . '/' . $route . '/' . rawurlencode((string) $row['slug']);
         $urls[] = [$location, $row['updated_at'] ?? null];
     }
 }
 
 header('Content-Type: application/xml; charset=utf-8');
 header('Cache-Control: no-cache, must-revalidate');
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
-foreach ($urls as [$location, $modified]) {
-    $escapedLocation = htmlspecialchars(
-        $location,
-        ENT_XML1 | ENT_QUOTES,
-        'UTF-8'
-    );
-    echo '  <url><loc>' . $escapedLocation . '</loc>';
-    if ($modified) {
-        $escapedModified = htmlspecialchars(
-            substr((string)$modified, 0, 10),
-            ENT_XML1,
-            'UTF-8'
-        );
-        echo '<lastmod>' . $escapedModified . '</lastmod>';
-    }
-    echo "</url>\n";
-}
-echo "</urlset>\n";
+echo brvtal_public_sitemap_xml($urls, $base);
