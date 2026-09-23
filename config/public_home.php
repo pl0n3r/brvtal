28d862d1bd723cb11f4e30890cd39106991049f9
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-22 20:09:50 -0500

    design: load Concept 05 Sound and Memories layer

--- config/public_home.php
+++ config/public_home.php
@@ -467,6 +467,20 @@ function brvtal_public_home_identity(string $html): string
             $html
         );
     }
+    if (!str_contains($html, 'css/public-concept05-sound-memories.css')) {
+        $html = str_replace(
+            '</head>',
+            "  <link rel=\"stylesheet\" href=\"css/public-concept05-sound-memories.css\">\n</head>",
+            $html
+        );
+    }
+    if (!str_contains($html, 'js/public-concept05-sound-memories.js')) {
+        $html = str_replace(
+            '</body>',
+            "  <script src=\"js/public-concept05-sound-memories.js\" defer></script>\n</body>",
+            $html
+        );
+    }
 
     if (!str_contains($html, 'css/public-home-phase-a.css')) {
         $html = str_replace(
