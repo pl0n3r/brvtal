a838c5ca3f63005f519984447058ec29faba3282
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-25 22:41:43 -0500

    refactor(blog): apply Rector-emitted quality changes (#653)

--- rector.php
+++ rector.php
@@ -2,13 +2,9 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\If_\ArrayExplicitBoolCompareRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPhpSets(php85: true)
     ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0)
-    ->withRules([
-        ArrayExplicitBoolCompareRector::class,
-    ]);
+    ->withCodeQualityLevel(0);
