17c72908a937e86a35f36d275452f9834310f1e8
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-26 16:35:12 -0500

    fix(artists): remove concurrent rollout scope

--- discadmin/content-core.js
+++ discadmin/content-core.js
@@ -315,6 +315,6 @@ const ready = (async()=>{try{await initAuth();await loadEvents({required:true});
   };
 })();
 
-Object.assign(window.BRVTALContentCore, {openEvent,closeEvent,loadEvents,loadArtists,addTicket,step,saveEvent,previewEvent,getCurrentEvent:()=>currentEvent});
+Object.assign(window.BRVTALContentCore, {openEvent,closeEvent,loadEvents,loadArtists,addTicket,step,saveEvent,previewEvent});
 return ready;
 }};
